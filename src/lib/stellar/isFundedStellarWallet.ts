c2471432fe255504b427a17d11cbb0bea522a268
douglasjr97 <falcao.douglas97@gmail.com>
2025-01-09 15:54:19 -0300

    refactor(NetworkWallet): implement condicional environment

--- src/lib/stellar/isFundedStellarWallet.ts
+++ src/lib/stellar/isFundedStellarWallet.ts
@@ -1,9 +1,14 @@
 import * as StellarSdk from "@stellar/stellar-sdk";
+import { isTestnet } from "@/lib/wallet/chains";
+
 
 export const checkIfWalletIsInitialized = async (address: string) => {
+  const testnet = process.env.NEXT_PUBLIC_TESTNET_NETWORK ?? ""
+  const publicNetwork = process.env.NEXT_PUBLIC_NETWORK ?? ""
+
   try {
     const horizonServer = new StellarSdk.Horizon.Server(
-      "https://horizon.stellar.org"
+      isTestnet ? testnet : publicNetwork
     );
     await horizonServer.loadAccount(address);
   } catch (error) {
