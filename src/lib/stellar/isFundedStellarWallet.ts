ba5b0abac66fc5225aa7c908d36c35b4928cf3ec
heronlancellot <heronlancellot@id.uff.br>
2025-05-30 17:16:33 -0300

    update: console.log to get error

--- src/lib/stellar/isFundedStellarWallet.ts
+++ src/lib/stellar/isFundedStellarWallet.ts
@@ -1,16 +1,19 @@
-import * as StellarSdk from "@stellar/stellar-sdk";
-import { isTestnet } from "@/lib/wallet/chains";
-
+import * as StellarSdk from '@stellar/stellar-sdk';
+import { isTestnet } from '@/lib/wallet/chains';
 
 export const checkIfWalletIsInitialized = async (address: string) => {
-  const testnet = process.env.NEXT_PUBLIC_TESTNET_NETWORK ?? ""
-  const publicNetwork = process.env.NEXT_PUBLIC_NETWORK ?? ""
+  const testnet = process.env.NEXT_PUBLIC_TESTNET_NETWORK ?? '';
+  const publicNetwork = process.env.NEXT_PUBLIC_NETWORK ?? '';
+
+  console.log('testnet', testnet);
+  console.log('publicNetwork', publicNetwork);
 
   try {
     const horizonServer = new StellarSdk.Horizon.Server(
       isTestnet ? testnet : publicNetwork
     );
     await horizonServer.loadAccount(address);
+    console.log('Wallet is funded');
   } catch (error) {
     throw error;
   }
